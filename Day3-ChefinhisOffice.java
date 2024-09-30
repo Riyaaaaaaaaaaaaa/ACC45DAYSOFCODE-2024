59b3fc7c011d60f2e722c7416d8e27dc018cf577
Riya Kushwah <120267693+Riyaaaaaaaaaaaaa@users.noreply.github.com>
2024-09-30 18:57:57 +0530

    Create Day3-ChefinhisOffice.java

---
+++ Day3-ChefinhisOffice.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-- >0)
+		{
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    
+		    int totalWorkingHours = (4*x)+y;
+		    System.out.println(totalWorkingHours);
+		}
+
+	}
+}
