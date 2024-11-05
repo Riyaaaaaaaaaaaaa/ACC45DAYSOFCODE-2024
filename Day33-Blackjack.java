f92fe6b76a9a314917723d254184654b196db7ba
Riya Kushwah <120267693+Riyaaaaaaaaaaaaa@users.noreply.github.com>
2024-11-05 18:45:04 +0530

    Create Day33-Blackjack.java

---
+++ Day33-Blackjack.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        while (t--> 0)
+        {
+            int a = s.nextInt();
+            int b = s.nextInt();
+            int c = 21 - (a + b);
+            if (c <= 10)
+                System.out.println(c);
+            else
+                System.out.println(-1);
+        }
+    }
+}
