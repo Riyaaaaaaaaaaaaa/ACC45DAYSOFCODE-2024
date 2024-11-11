e352974e8394a7de554518bd955c0e8248eeb7e8
Riya Kushwah <120267693+Riyaaaaaaaaaaaaa@users.noreply.github.com>
2024-11-11 18:23:06 +0530

    Create Day40-Watching Movies at 2x.java

---
+++ Day40-Watching Movies at 2x.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
+        System.out.println(n - m / 2);
+    }
+}
